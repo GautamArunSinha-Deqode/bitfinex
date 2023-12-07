e461a0a675e9baf940276adac309f2f112ededda
GautamArunSinha-Deqode <gsinha@deqode.com>
2023-12-07 16:40:09 +0530

    Order Book Helper (Dec-07-2023)

--- src/component/OrderBook.tsx
+++ src/component/OrderBook.tsx
@@ -4,13 +4,10 @@
 import React, { useEffect, useState } from "react";
 // import { OrderBook } from "@lab49/react-order-book";
 import { w3cwebsocket as W3CWebSocket } from "websocket";
-
-
-
+import {numberWithCommas} from "../helper/NumberSaprator"
 
 
 const OrderBookCom = () => {
-  
   interface OrderBookData {
     asks: OrderBookEntry[];
     bids: OrderBookEntry[];
@@ -22,7 +19,6 @@ const OrderBookCom = () => {
     total: number;
     count: number;
   }
-  
 
   const [orderBookData, setOrderBookData] = useState<OrderBookData>({
     asks: [],
@@ -37,9 +33,10 @@ const OrderBookCom = () => {
     const orderBooksSubscription = {
       event: "subscribe",
       channel: "book",
-      // symbol: 'tBTCUSD',
-      symbol: "fUSD",
-      prec: "P4", // Precision, you may need to adjust this based on your requirements
+      freq: "F1",
+      len: "25",
+      symbol: "BTCUSD",
+      prec: "P0", // Precision, you may need to adjust this based on your requirements
     };
 
     // Create a WebSocket connection
@@ -51,68 +48,78 @@ const OrderBookCom = () => {
       ws.send(JSON.stringify(orderBooksSubscription));
     };
 
-  
     ws.onmessage = (message) => {
       const data = JSON.parse(message?.data.toString());
-      console.log("datatypeof" , typeof data)
+      console.log("datatypeof", data);
+
       if (data && Array.isArray(data) && data[1]) {
         const [channelId, orderBookEntries] = data;
-  
+
         if (
           channelId &&
           orderBookEntries &&
           typeof orderBookEntries === "object"
         ) {
-          // If the snapshot flag is present, reset the order book data
-  
-
           const isSnapshot = Object.keys(orderBookEntries).length > 4;
-          console.log("isSnapshot isSnapshot",  orderBookEntries);
-         
+
           if (isSnapshot) {
+            // Snapshot
             setOrderBookData({
-              asks: orderBookEntries.filter((entry:any) => entry[2] > 0),
-              bids: orderBookEntries.filter((entry:any) => entry[2] < 0),
+              asks: orderBookEntries
+                .filter((entry: any) => entry[2] > 0)
+                .map((entry: any) => [
+                  parseFloat(entry[0]),
+                  entry[1],
+                  entry[1] * parseFloat(entry[0]),
+                  entry[2],
+                ]),
+              bids: orderBookEntries
+                .filter((entry: any) => entry[2] < 0)
+                .map((entry: any) => [
+                  parseFloat(entry[0]),
+                  entry[1],
+                  entry[1] * parseFloat(entry[0]),
+                  entry[2],
+                ]),
             });
           } else {
-
-          console.log("isSnapshot isSnapshot",  orderBookEntries);
-
-            // Update the state with the new order book data
+            // Update
             setOrderBookData((prevOrderBookData) => {
               const updatedAsks: any[] = [...prevOrderBookData.asks];
               const updatedBids: any[] = [...prevOrderBookData.bids];
 
               Object.keys(orderBookEntries).forEach((price) => {
-                const entry = orderBookEntries[price];
-                const size = entry[0];
-                const count = entry[1];
-                const updatedEntry = [
-                  parseFloat(price),
-                  size,
-                  size * parseFloat(price),
-                  count,
-                ];
+                if (!isNaN(parseFloat(price))) {
+                  const entry = orderBookEntries[price];
+                  const size = entry[0];
+                  const count = entry[1];
+                  const updatedEntry = [
+                    parseFloat(price),
+                    size,
+                    size * parseFloat(price),
+                    count,
+                  ];
 
-                if (size > 0) {
-                  // Ask
-                  const index = updatedAsks.findIndex(
-                    (ask) => ask[0] === parseFloat(price)
-                  );
-                  if (index !== -1) {
-                    updatedAsks[index] = updatedEntry;
-                  } else {
-                    updatedAsks.push(updatedEntry);
-                  }
-                } else if (size < 0) {
-                  // Bid
-                  const index = updatedBids.findIndex(
-                    (bid) => bid[0] === parseFloat(price)
-                  );
-                  if (index !== -1) {
-                    updatedBids[index] = updatedEntry;
-                  } else {
-                    updatedBids.push(updatedEntry);
+                  if (size > 0) {
+                    // Ask
+                    const index = updatedAsks.findIndex(
+                      (ask) => ask[0] === parseFloat(price)
+                    );
+                    if (index !== -1) {
+                      updatedAsks[index] = updatedEntry;
+                    } else {
+                      updatedAsks.push(updatedEntry);
+                    }
+                  } else if (size < 0) {
+                    // Bid
+                    const index = updatedBids.findIndex(
+                      (bid) => bid[0] === parseFloat(price)
+                    );
+                    if (index !== -1) {
+                      updatedBids[index] = updatedEntry;
+                    } else {
+                      updatedBids.push(updatedEntry);
+                    }
                   }
                 }
               });
@@ -145,58 +152,60 @@ const OrderBookCom = () => {
     return () => {
       ws.close();
     };
-  }, []);
+  }, [orderBookData]);
 
-  const orderRows = (arr:any) =>
+  const orderRows = (arr: any) =>
     arr &&
-    arr.map((item :any, index:number) => (
-      <tr key={index}>
-        <td> {item[0]} </td>
+    arr.map((item: any, index: number) => (
+      <tr>
         <td> {item[1]} </td>
-        <td> {item[2]} </td>
+        <td> {item[2].toFixed(2)} </td>
         <td> {item[3]} </td>
+        <td> {numberWithCommas(item[0])} </td>
       </tr>
     ));
-  const orderHead = (title:string) => (
+
+    // Price: entry[0]
+    // Count: entry[1]
+    // Amount: entry[2]
+    // Total: entry[3]
+  const orderHead = (title: string) => (
     <thead>
       <tr>
-        <th >{title}</th>
+        {/* <th>{title}</th> */}
       </tr>
       <tr>
-        <th>Price </th>
+        {/* <th>Price </th>
         <th>Size </th>
         <th>Total </th>
+        <th>Count </th> */}
+
         <th>Count </th>
+        <th>Amount </th>
+        <th>Total </th>
+        <th>Price </th>
       </tr>
     </thead>
   );
 
-  console.log(
-    "orderBookDataorderBookDataorderBookDataorderBookData",
-    orderBookData
-  );
-
   return (
     <div>
-      {/* //   <h2>Order Book</h2>
-  //   <OrderBookTable bidOrders={bidOrders} askOrders={askOrders} /> */}
-
       <h2>Order Book </h2>
 
       <div className="order-container">
-        {/* {bidOrders?.length > 0  ? ( */}
         <>
           <table>
             {orderHead("Bids")}
             <tbody>{orderRows(orderBookData.bids)}</tbody>
           </table>
-
+          &nbsp; &nbsp; &nbsp; &nbsp;
           <table>
             {orderHead("Asks")}
-            <tbody>{orderRows(orderBookData.asks)}</tbody>
+            <tbody>
+              
+              {orderRows(orderBookData.asks)}</tbody>
           </table>
         </>
-        {/* ) : ""} */}
       </div>
     </div>
   );
